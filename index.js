271859c4fc91b7f30880dd2de8ac3fcb48e2e845
lando2319 <mikepland@gmail.com>
2018-06-02 11:42:25 -0500

    initial setup on drop tester

---
+++ index.js
@@ -0,0 +1,46 @@
+
+
+const async = require("async");
+// const funcitons = require("firebase-functions");
+const admin = require("firebase-admin");
+const configActual = require("./config/configActual.js");
+const util = require('util');
+
+admin.initializeApp({
+    credential: admin.credential.cert(configActual.pwdIs),
+    databaseURL: configActual.dbURL
+});
+
+const firestore = admin.firestore();
+const deleteActual = admin.firestore.FieldValue.delete();
+
+var userQueue = async.queue(function(userDocID, finalCallback) {
+    console.log("\n>>>> Beginning Process For " + userDocID);
+
+    firestore.collection("users").doc(userDocID).set({countIs:deleteActual}).then(function() {
+        console.log("Successfully Saved User Info to Database");
+        finalCallback();
+    }).catch(function(error) {
+        console.log("Firebase Error: " + error);
+        process.exit(1);
+    });
+
+}, 1);
+
+userQueue.drain = function() {
+    console.log("----------------------------------- SCRIPT COMPLETE -----------------------------------");
+    process.exit(1);
+};
+
+function startProcess() {
+    var i;
+    for (i = 0; i < 1001; i++) {
+        var userDocID = "zzzzzzzzzzzzzzzzzzzzzzzzzzzz" + i;
+        userQueue.push(userDocID);
+    }
+};
+
+// ------------------ START PROCESS ------------------ 
+startProcess();
+
+
