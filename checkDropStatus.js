9573fbb1590c60a0231f8fe371db59180ff5616c
lando2319 <mikepland@gmail.com>
2018-06-02 13:45:29 -0500

    updates results log, adds counter on checkDropped

--- checkDropStatus.js
+++ checkDropStatus.js
@@ -11,11 +11,16 @@ admin.initializeApp({
 
 var fireDB = admin.firestore();
 
+var recordCount = 0;
+
 fireDB.collection("users").where("countThis", "<", "DONE").get().then(snapshot => {
     snapshot.forEach(doc => {
         console.log("uid", doc.id);
         console.log("countThis", doc.data().countThis);
+        recordCount ++;
     });
+
+    console.log("recordCount", recordCount);
 }).catch(err => {
     console.log('Error getting documents', err);
     process.exit(1);
